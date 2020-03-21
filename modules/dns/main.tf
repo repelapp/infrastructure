86879fed77a82889bf63a7c899d4cc692d150469
Jan Weitz <me@janweitz.de>
2020-03-21 14:30:23 +0100

    Only www.repealapp.de

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -8,19 +8,7 @@ locals {
 
 }
 
-# repelapp.de
-resource "aws_route53_record" "domain" {
-  zone_id = data.aws_route53_zone.zone.zone_id
-
-  name = "${data.aws_route53_zone.zone.name}"
-  type = "CNAME"
-  ttl  = "300"
-  records = [
-    local.github_pages_domain,
-  ]
-}
-
-# repelapp.de
+# www.repelapp.de
 resource "aws_route53_record" "www" {
   zone_id = data.aws_route53_zone.zone.zone_id
 
