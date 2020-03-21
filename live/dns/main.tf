d9d50fece32c70696d6de7046af3ae3b01ed5df5
Jan Weitz <jan.weitz@devk.de>
2020-03-21 14:14:09 +0100

    Add github pages domain

---
+++ live/dns/main.tf
@@ -0,0 +1,34 @@
+data "aws_route53_zone" "zone" {
+  name         = "repelapp.de."
+  private_zone = false
+}
+
+locals {
+  github_pages_domain = "repelapp.github.io"
+
+}
+
+# repelapp.de
+resource "aws_route53_record" "domain" {
+  zone_id = "${data.aws_route53_zone.zone.zone_id}"
+
+  name = "${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  ttl  = "300"
+  records = [
+    local.github_pages_domain,
+  ]
+}
+
+# repelapp.de
+resource "aws_route53_record" "www" {
+  zone_id = "${data.aws_route53_zone.zone.zone_id}"
+
+  name = "www.${data.aws_route53_zone.zone.name}"
+  type = "CNAME"
+  ttl  = "300"
+  records = [
+    local.github_pages_domain,
+  ]
+}
+
